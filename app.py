baca0bed25a0920461ddcab11c45ef9f6148d2ba
cchiriac518 <cchiriac518@gmail.com>
2020-06-24 13:26:12 +0300

    Added version route and port variable

--- app.py
+++ app.py
@@ -22,3 +22,15 @@ def sysinfo():
     else:
         cp = subprocess.run('lscpu', capture_output=True, text=True, shell=True)
         return cp.stdout
+@app.route('/version', methods=['POST', 'GET'])
+def OSver():
+     systype = platform.system()
+     if systype == 'Windows':
+          osver = subprocess.run('ver', capture_output=True, text=True, shell=True)
+          return osver.stdout
+     else:
+          lver = subprocess.run('uname -r', capture_output=True, text=True, shell=True)
+          return ("Kernel version: " + lver.stdout)
+
+if __name__ == "__main__":
+     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))

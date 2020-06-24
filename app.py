be7e29ac48253afe43be48f943abddd2b9c96b79
cchiriac518 <cchiriac518@gmail.com>
2020-06-24 13:21:53 +0300

    Added hostname route

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route('/', methods=['POST','GET'])
 def index():
      return 'Welcome to my first API/container'
+
+@app.route('/hostname', methods=['POST','GET'])
+def hostnm():
+    hn = subprocess.run('hostname',capture_output=True,text=True,shell=True)
+    return hn.stdout
